f865557c891dceae8027dcc5125a9c0e66ce3dfd
EricWRogers <ericwilliamrogers@gmail.com>
2023-06-10 20:42:59 -0500

    Enemy Turrets

--- Assets/Scripts/Turret/Turret.cs
+++ Assets/Scripts/Turret/Turret.cs
@@ -7,16 +7,57 @@ public class Turret : MonoBehaviour
     public GameObject target;
     public TurretMount mount;
     public TurretAxle axle;
+    public Transform firePoint;
+    public float range;
+    public float accurecy = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameManager.Instance.GetPlayer().gameObject;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         mount.FaceTarget(target.transform.position);
         axle.FaceTarget(target.transform.position);
+    }
+
+    public void Fire()
+    {
+        Debug.Log("Fire");
+        if (Vector3.Distance(target.transform.position, firePoint.position) > range)
+            return;
+        Debug.Log("Bang");
+        GameObject bullet = SuperPupSystems.Helper.SimpleObjectPool.Instance.SpawnFromPool(
+            "enemy_bullet",
+            firePoint.position,
+            Quaternion.identity
+        );
+
+        Vector3 positionOnPoint = RandomPointOnPlane(
+            target.transform.position,
+            firePoint.forward,
+            accurecy
+        );
+
+        bullet.transform.LookAt(positionOnPoint);
+    }
+
+    private Vector3 RandomPointOnPlane(Vector3 position, Vector3 normal, float radius)
+    {
+        Vector3 randomPoint;
+
+        do
+        {
+            randomPoint = Vector3.Cross(Random.insideUnitSphere, normal);
+        } while (randomPoint == Vector3.zero);
+
+        randomPoint.Normalize();
+        randomPoint *= radius;
+        randomPoint += position;
+
+        return randomPoint;
     }
 }

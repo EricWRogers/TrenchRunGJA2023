1673e1516fecbc8118e1eed22ffd3f4d5cd5facf
BizarreFog <bizarrefog@gmail.com>
2023-06-10 19:51:38 -0500

    Initial Retical

---
+++ Assets/Scripts/Managers/InGameUIManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUIManager : SingletonMonoBehaviour<InGameUIManager>
+{
+    public GameObject targetingRetical;
+    public GameObject subTargetingRetical;
+
+
+}
